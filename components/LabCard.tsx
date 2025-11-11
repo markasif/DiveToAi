28afecf98add6ad387b679c87a4603eb18ac9003
markasif <apasif243@gmail.com>
2025-11-11 10:32:37 +0530

    done

--- components/LabCard.tsx
+++ components/LabCard.tsx
@@ -19,12 +19,12 @@ const GridItem: React.FC<GridItemProps> = ({
       className={`relative w-full overflow-hidden rounded-2xl shadow-lg bg-cover bg-center ${className}`}
       style={{ backgroundImage: `url(${imageUrl})` }}
     >
-      <div className="absolute inset-0 bg-black/30 transition-opacity"></div>
+      <div className="absolute inset-0 bg-black/30"></div>
 
       <div
         className="relative z-10 flex h-full w-full p-20 items-center justify-center text-center"
       >
-        <h3 className="text-xl md:text-2xl font-cal text-white">{title}</h3>
+        <h3 className="text-[28px] md:text-[20px] lg:text-[28px] xl:text-[30px] font-cal text-white">{title}</h3>
       </div>
     </div>
   );
